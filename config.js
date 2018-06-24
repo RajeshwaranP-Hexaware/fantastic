976c006bdf0b0fa95e98e782c02427f7b7609678
RajeshwaranP-Hexaware <rajeshfederer2@gmail.com>
2018-06-24 07:40:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {	
-	accessToken: "aa3c4bf166e14cdbaa1d46d3a3fbe4b4",
+	accessToken: "18bc30c20f82483d82559bb1b7506d27",
     dialogflowAPI : "https://api.api.ai/v1/query?v=20150910"
 }
 
